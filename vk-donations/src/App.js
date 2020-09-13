e71912fb7d44c5d748b3387ede2966a7fe522a15
mark-nikerin <mrnikerin@gmail.com>
2020-09-14 00:23:44 +0400

    First big commit

--- vk-donations/src/App.js
+++ vk-donations/src/App.js
@@ -1,25 +1,316 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import {
+  View,
+  Panel,
+  PanelHeader,
+  PanelHeaderBack,
+  Group,
+  Button,
+  Placeholder,
+  Banner,
+  FormLayout,
+  Div,
+  File,
+  Input,
+  Select,
+  Textarea,
+  FormLayoutGroup,
+  Radio,
+} from "@vkontakte/vkui";
+import "@vkontakte/vkui/dist/vkui.css";
+import Icon28TargetOutline from "@vkontakte/icons/dist/28/target_outline";
+import Icon28CalendarOutline from "@vkontakte/icons/dist/28/calendar_outline";
+import Icon56GalleryOutline from "@vkontakte/icons/dist/56/gallery_outline";
+
+import "./App.css";
 
 function App() {
+  const [currentPanel, setCurrentPanel] = React.useState("create");
+  const [showDate, setShowDate] = React.useState(false);
+  const [isReady, setIsReady] = React.useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <View activePanel={currentPanel}>
+      <Panel id="create" centered="true">
+        <PanelHeader>Пожертвования</PanelHeader>
+        <Group>
+          <Placeholder
+            action={
+              <Button size="m" onClick={() => setCurrentPanel("selectType")}>
+                Создать сбор
+              </Button>
+            }
+          >
+            У Вас пока нет сборов.
+            <br />
+            Начните доброе дело.
+          </Placeholder>
+        </Group>
+      </Panel>
+      <Panel id="selectType" centered="true">
+        <PanelHeader
+          left={<PanelHeaderBack onClick={() => setCurrentPanel("create")} />}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Тип сбора
+        </PanelHeader>
+        <Group>
+          <Banner
+            before={<Icon28TargetOutline fill="3F8AE0" />}
+            header="Целевой сбор"
+            subheader="Когда есть определённая цель"
+            asideMode="expand"
+            onClick={() => setCurrentPanel("targetedDonation")}
+          />
+          <Banner
+            before={<Icon28CalendarOutline fill="3F8AE0" />}
+            header="Регулярный сбор"
+            subheader="Если помощь нужна ежемесячно"
+            asideMode="expand"
+            onClick={() => setCurrentPanel("regularDonation")}
+          />
+        </Group>
+      </Panel>
+      <Panel id="targetedDonation">
+        <PanelHeader
+          left={
+            <PanelHeaderBack onClick={() => setCurrentPanel("selectType")} />
+          }
+        >
+          Целевой сбор
+        </PanelHeader>
+        <Group>
+          <FormLayout>
+            <Banner
+              mode="image"
+              size="m"
+              background={
+                <div
+                  style={{
+                    boxSizing: "border-box",
+                    backgroundColor: "#ffffff",
+                    borderRadius: "9px",
+                    border: "1.5px dashed #3F8AE0",
+                  }}
+                />
+              }
+              actions={
+                <Group align="center">
+                  <Div style={{ marginTop: "0px" }} />
+                  <File
+                    mode="tertiary"
+                    before={
+                      <Icon56GalleryOutline
+                        wifth={28}
+                        height={28}
+                        fill="3F8AE0"
+                      />
+                    }
+                  >
+                    Загрузить обложку
+                  </File>
+                  <Div />
+                </Group>
+              }
+            />
+            <Input
+              type="text"
+              top="Название сбора"
+              placeholder="Название сбора"
+            />
+            <Input
+              type="number"
+              top="Сумма, ₽"
+              placeholder="Cколько нужно собрать?"
+            />
+            <Input
+              type="text"
+              top="Цель"
+              placeholder="Например, лечение человека"
+            />
+            <Textarea
+              top="Описание"
+              placeholder="На что пойдут деньги и как они кому-то помогут?"
+              onChange={(event) => {
+                event.target.value !== "" && event.target.value !== null
+                  ? setIsReady(true)
+                  : setIsReady(false);
+              }}
+            />
+            <Select top="Куда получать деньги">
+              <option value="first">Счет VK Pay • 1234</option>
+              <option value="second">Счет VK Pay • 5678</option>
+            </Select>
+          </FormLayout>
+          <Div>
+            {isReady ? (
+              <Button
+                size="xl"
+                onClick={() => {
+                  setCurrentPanel("targetedDonationAdditional");
+                }}
+              >
+                Далее
+              </Button>
+            ) : (
+              <Button style={{ backgroundColor: "#B6CFEB" }} size="xl">
+                Далее
+              </Button>
+            )}
+          </Div>
+          <Div />
+        </Group>
+      </Panel>
+      <Panel id="targetedDonationAdditional">
+        <PanelHeader
+          left={
+            <PanelHeaderBack
+              onClick={() => {
+                setCurrentPanel("targetedDonation");
+              }}
+            />
+          }
+        >
+          Дополнительно
+        </PanelHeader>
+        <Group>
+          <FormLayout>
+            <Select top="Автор">
+              <option value="first">Матвей Правосудов</option>
+              <option value="second">Марк Никерин</option>
+            </Select>
+            <FormLayoutGroup top="Сбор завершится">
+              <Radio
+                type="radio"
+                name="radio"
+                defaultChecked="true"
+                onChange={(event) => {
+                  setShowDate(!event.target.checked);
+                }}
+              >
+                Когда соберём сумму
+              </Radio>
+              <Radio
+                type="radio"
+                name="radio"
+                onChange={(event) => {
+                  setShowDate(event.target.checked);
+                  setIsReady(false);
+                }}
+              >
+                В определенную дату
+              </Radio>
+            </FormLayoutGroup>
+            {showDate && (
+              <Select
+                top="Дата окончания"
+                placeholder="Выберите дату"
+                onChange={() => setIsReady(true)}
+              >
+                <option value="first">20 сентября</option>
+                <option value="first">30 сентября</option>
+              </Select>
+            )}
+          </FormLayout>
+          <Div>
+            {isReady ? (
+              <Button
+                size="xl"
+                onClick={() => {
+                  setCurrentPanel("selectType");
+                  setIsReady(false);
+                }}
+              >
+                Создать сбор
+              </Button>
+            ) : (
+              <Button style={{ backgroundColor: "#B6CFEB" }} size="xl">
+                Создать сбор
+              </Button>
+            )}
+          </Div>
+          <Div />
+        </Group>
+      </Panel>
+      <Panel id="regularDonation">
+        <PanelHeader
+          left={
+            <PanelHeaderBack onClick={() => setCurrentPanel("selectType")} />
+          }
+        >
+          Регулярный сбор
+        </PanelHeader>
+        <Group>
+          <FormLayout>
+            <Banner
+              mode="image"
+              size="m"
+              background={
+                <div
+                  style={{
+                    boxSizing: "border-box",
+                    backgroundColor: "#ffffff",
+                    borderRadius: "9px",
+                    border: "1.5px dashed #3F8AE0",
+                  }}
+                />
+              }
+              actions={
+                <Group align="center">
+                  <Div style={{ marginTop: "0px" }} />
+                  <File
+                    mode="tertiary"
+                    before={
+                      <Icon56GalleryOutline
+                        wifth={28}
+                        height={28}
+                        fill="3F8AE0"
+                      />
+                    }
+                  >
+                    Загрузить обложку
+                  </File>
+                  <Div />
+                </Group>
+              }
+            />
+            <Input
+              type="text"
+              top="Название сбора"
+              placeholder="Название сбора"
+            />
+            <Input
+              type="number"
+              top="Сумма, ₽"
+              placeholder="Cколько нужно в месяц?"
+            />
+            <Input
+              type="text"
+              top="Цель"
+              placeholder="Например, поддержка приюта"
+            />
+            <Textarea
+              top="Описание"
+              placeholder="На что пойдут деньги и как они кому-то помогут?"
+            />
+            <Select top="Куда получать деньги">
+              <option value="first">Счет VK Pay • 1234</option>
+              <option value="second">Счет VK Pay • 5678</option>
+            </Select>
+            <Select top="Автор">
+              <option value="first">Матвей Правосудов</option>
+              <option value="second">Марк Никерин</option>
+            </Select>
+          </FormLayout>
+          <Div>
+            <Button size="xl" onClick={() => setCurrentPanel("selectType")}>
+              Далее
+            </Button>
+          </Div>
+          <Div />
+        </Group>
+      </Panel>
+    </View>
   );
 }
 
