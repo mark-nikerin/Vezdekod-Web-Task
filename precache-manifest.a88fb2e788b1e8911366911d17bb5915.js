2e9dc6e8f53a63c1b0c8f04254f63e7c144cad91
mark-nikerin <mrnikerin@gmail.com>
2020-09-14 01:02:28 +0400

    Updates

---
+++ precache-manifest.a88fb2e788b1e8911366911d17bb5915.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cff6dd6fe0a273bcf3147c77874727a",
+    "url": "/Vezdekod-Web-Task/index.html"
+  },
+  {
+    "revision": "f8573f73c59cef650698",
+    "url": "/Vezdekod-Web-Task/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "f2a3a6a587643dd7e64f",
+    "url": "/Vezdekod-Web-Task/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f8573f73c59cef650698",
+    "url": "/Vezdekod-Web-Task/static/js/2.f68fe705.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Vezdekod-Web-Task/static/js/2.f68fe705.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2a3a6a587643dd7e64f",
+    "url": "/Vezdekod-Web-Task/static/js/main.4e561325.chunk.js"
+  },
+  {
+    "revision": "d38ac73a7b849657264d",
+    "url": "/Vezdekod-Web-Task/static/js/runtime-main.0c244d19.js"
+  },
+  {
+    "revision": "e22c4f7ce9d7c9e610ecdca2badde3d6",
+    "url": "/Vezdekod-Web-Task/static/media/image.e22c4f7c.png"
+  }
+]);
